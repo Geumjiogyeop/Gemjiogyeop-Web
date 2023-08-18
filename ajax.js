55ef4cb8c9e59c1cf5051a9f5bcb8a9dbbb3f01f
sycuuui <102959791+sycuuui@users.noreply.github.com>
2023-08-18 22:09:44 +0900

    ajax 코드 수정

--- ajax.js
+++ ajax.js
@@ -1,71 +1,47 @@
-$("#submitBtn").click(function(){
+document.getElementById("submitBtn").addEventListener("click", function () {
     check();
-    console.log("isSubmit:"+isSubmit);
-    if(isSubmit=="true"){
-        if(type=="type-etc"){
-            $.ajax({
-                type : 'post',           // 타입 (get, post, put 등등)
-                url : 'http://175.45.194.93/report/',           // 요청할 서버url
-                async : true,            // 비동기화 여부 (default : true)
-                headers : {              // Http header
-                  "Content-Type" : "application/json",
-                  "X-HTTP-Method-Override" : "POST"
-                },
-                dataType : 'json',       // 데이터 타입 (html, xml, json, text 등등)
-                data : JSON.stringify({  // 보낼 데이터 (Object , String, Array)
-                    "name" : username,
-                  "relation" : realationship,
-                  "victim_id" : userID,
-                  "date" : date,
-                  "type" : type,
-                  "etc" : etc
-                }),
-                success : function(result) { // 결과 성공 콜백함수
-                    console.log(result);
-                },
-                error : function(request, status, error) { // 결과 에러 콜백함수
-                    console.log(error)
-                }
-            })
-            console.log("통신 코드 성공");
-            console.log("realationship:"+realationship);
-            console.log("userID:"+userID);
-            console.log("date:"+date);
-            console.log("type:"+type);
-            console.log("etc:"+etc);
+    console.log("isSubmit:" + isSubmit);
 
-            alert("신고접수 되었습니다.")
-        }else{
-            $.ajax({
-                type : 'post',           // 타입 (get, post, put 등등)
-                url : 'http://175.45.194.93/report/',           // 요청할 서버url
-                async : true,            // 비동기화 여부 (default : true)
-                headers : {              // Http header
-                  "Content-Type" : "application/json",
-                  "X-HTTP-Method-Override" : "POST"
-                },
-                dataType : 'json',       // 데이터 타입 (html, xml, json, text 등등)
-                data : JSON.stringify({  // 보낼 데이터 (Object , String, Array)
-                    "name" : username,
-                  "relation" : realationship,
-                  "victim_id" : userID,
-                  "date" : date,
-                  "type" : type
-                }),
-                success : function(result) { // 결과 성공 콜백함수
-                    console.log(result);
-                },
-                error : function(request, status, error) { // 결과 에러 콜백함수
-                    console.log(error)
-                }
-            })
-            console.log("realationship:"+realationship);
-            console.log("userID:"+userID);
-            console.log("date:"+date);
-            console.log("type:"+type);
-            alert("신고접수 되었습니다.")
+    if (isSubmit === "true") {
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "http://175.45.194.93/report/", true);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        
+        var data = {
+            "name": username,
+            "relation": realationship,
+            "victim_id": userID,
+            "date": date,
+            "type": type
+        };
+
+        if (type === "type-etc") {
+            data.etc = etc;
         }
-    }else{
-        console.log("isSubmit:"+isSubmit+"상태이므로 오류");
+
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                console.log(xhr.responseText);
+                console.log("통신 코드 성공");
+                console.log("realationship:" + realationship);
+                console.log("userID:" + userID);
+                console.log("date:" + date);
+                console.log("type:" + type);
+                if (type === "type-etc") {
+                    console.log("etc:" + etc);
+                }
+                alert("신고접수 되었습니다.");
+            } else {
+                console.error("Request failed with status:", xhr.status);
+            }
+        };
+
+        xhr.onerror = function () {
+            console.error("Request failed");
+        };
+
+        xhr.send(JSON.stringify(data));
+    } else {
+        console.log("isSubmit:" + isSubmit + "상태이므로 오류");
     }
 });
